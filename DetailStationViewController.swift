9256dd8b89d60c08b997716cc2203a0184412dc3
kassiane mentz <kassimentz@gmail.xom>
2016-10-29 16:52:15 -0200

    add details to each station

--- DetailStationViewController.swift
+++ DetailStationViewController.swift
@@ -12,10 +12,55 @@ class DetailStationViewController: UIViewController {
 
     var weatherStation: WeatherStation?
     
+    @IBOutlet weak var lblEstacao: UILabel!
+    @IBOutlet weak var lblTemperatura: UILabel!
+    @IBOutlet weak var lblSensacaoTermica: UILabel!
+    @IBOutlet weak var lblTemperaturaMaxima: UILabel!
+    @IBOutlet weak var lblTemperaturaMinima: UILabel!
+    
+    
+    func getDetails() {
+    
+        if let weatherStation = weatherStation {
+            
+            if let estacao = weatherStation.station {
+                lblEstacao.text = "Estação: \(estacao)"
+            } else {
+                lblEstacao.text = "Estação não informada"
+            }
+            
+            if let temperatura = weatherStation.temperaturaExterna {
+                lblTemperatura.text = "Temperatura: \(temperatura)"
+            } else {
+                lblTemperatura.text = "Temperatura não informada"
+            }
+            
+            if let sensacao = weatherStation.sensacaoTermica {
+                lblSensacaoTermica.text = "Sensação Térmica: \(sensacao)"
+            } else {
+                lblSensacaoTermica.text = "Sensação Térmica não informada"
+            }
+            
+            if let maxima = weatherStation.temperaturaMaximaPrevisao {
+                lblTemperaturaMaxima.text = "Máxima Prevista: \(maxima)"
+            } else {
+                lblTemperaturaMaxima.text = "Máxima Prevista não informada"
+            }
+            
+            if let minima = weatherStation.temperaturaMinimaPrevisao {
+                lblTemperaturaMinima.text = "Mínima Prevista: \(minima)"
+            } else {
+                lblTemperaturaMinima.text = "Mínima Prevista não informada"
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.title = "Detalhes"
+        
+        getDetails()
         //weatherStation?.station
         // Do any additional setup after loading the view.
     }

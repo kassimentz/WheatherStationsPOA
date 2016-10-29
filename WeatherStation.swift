be0b0900b17348e3ccd92dcef46689e566f3f92f
kassiane mentz <kassimentz@gmail.xom>
2016-10-29 11:58:08 -0200

    Load meteorologic stations from json dataPoa

---
+++ WeatherStation.swift
@@ -0,0 +1,21 @@
+//
+//  WeatherStation.swift
+//  WheatherStationsPOA
+//
+//  Created by iossenac on 29/10/16.
+//  Copyright © 2016 Kassiane Mentz. All rights reserved.
+//
+
+import UIKit
+
+//pojo
+
+struct WeatherStation {
+    let station: String?
+    let latitude: Double?
+    let longitude: Double?
+    let temperaturaExterna: Double?
+    let sensacaoTermica: Double?
+    let temperaturaMinimaPrevisao: Double?
+    let temperaturaMaximaPrevisao: Double?
+}
---
+++ WeatherStationManager.swift
@@ -0,0 +1,47 @@
+//
+//  WeatherStationManager.swift
+//  WheatherStationsPOA
+//
+//  Created by iossenac on 29/10/16.
+//  Copyright © 2016 Kassiane Mentz. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+//faz o acesso à inclusao no meu model. trará um array de estacao
+class WeatherStationManager: NSObject {
+    
+    // MARK: - Closures (ou bloco de instrucao)
+    
+    func loadWeatherStations(callback: @escaping(
+        _ weatherStations: [WeatherStation]?, _ error: Error?) -> ()){
+        
+        Alamofire.request("https://metroclimaestacoes.procempa.com.br/metroclima/seam/resource/rest/externalRest/ultimaLeitura").responseJSON {response
+            in
+            
+            var weatherStations = [WeatherStation]()
+            
+            let json = JSON(data: response.data!)
+            
+            for (_, subJSON): (String, JSON) in json {
+                
+                let weatherStation = WeatherStation(station: subJSON["estacao"].string,
+                                                    latitude: subJSON["latitude"].double,
+                                                    longitude: subJSON["latitude"].double,
+                                                    temperaturaExterna: subJSON["temperaturaExterna"].double,
+                                                    sensacaoTermica: subJSON["sensacaoTermica"].double,
+                                                    temperaturaMinimaPrevisao: subJSON["temperaturaMinimaPrevisao"].double,
+                                                    temperaturaMaximaPrevisao: subJSON["temperaturaMaximaPrevisao"].double)
+                
+                    weatherStations.append(weatherStation)
+                
+            }
+            
+            callback(weatherStations, json.error)
+            
+        }
+    }
+
+}
